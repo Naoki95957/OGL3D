ff4e6055a9a4a221353c95f6ae67f9ab93d14653
Naoki95957 <naoki.l.95957@gmail.com>
2015-02-05 00:24:33 -0800

    texturing now implemented

--- src/engine/Main.java
+++ src/engine/Main.java
@@ -1,12 +1,15 @@
 package engine;
 
+import models.RawModel;
+import models.TexturedModel;
+
 import org.lwjgl.opengl.Display;
 
 import render.DisplayManagement;
 import render.Loader;
-import render.RawModel;
 import render.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class Main {
 
@@ -29,13 +32,21 @@ public class Main {
 			0,1,3,	//Top left triangle (v0,v1,v3)
 			3,1,2	//bottom right triangle (v3,v1,v2)
 		};
+		float textureCoords []={
+			0,0,	//V0
+			0,1,	//V1
+			1,1,	//V2
+			1,0		//V3
+		};
 		
-		RawModel model = loader.loadToVAO(verticies, indices);
+		RawModel model = loader.loadToVAO(verticies, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("weave"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
 		while(!Display.isCloseRequested()){
 			renderer.prepare();
 			shader.start();
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManagement.updateDisplay();
 			
